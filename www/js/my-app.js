1bce572d2ebd36ee2e98b8738017c88b4592c293
Alan Felix <alan22fnc@gmail.com>
2017-05-21 12:31:20 -0300

    first commit

---
+++ www/js/my-app.js
@@ -0,0 +1,26 @@
+// Initialize app
+var myApp = new Framework7({
+	swipePanel: 'left'
+});
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Dispositivo pronto!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('add-desp', function (page) {
+});
